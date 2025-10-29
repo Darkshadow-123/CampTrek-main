f849b68027e2fe4f0509511833243bb51bdc7443
Rishi Lalwani <Rishi.123.lalwani@gmail.com>
2025-10-29 10:47:59 +0530

    mongodb changes

--- seeds/index.js
+++ seeds/index.js
@@ -28,7 +28,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 200);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-            author: '673ec7af1e73754a0c342b13',
+            author: '6900f5b30456a15964c8ce05',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             geometry: {
